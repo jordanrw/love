7959419276c005bbf91c7410baf001fc606a52f7
Jordan White <jordanrw@vt.edu>
2015-02-07 18:01:52 -0500

    updated location support

--- love/ViewController.m
+++ love/ViewController.m
@@ -8,15 +8,72 @@
 
 #import "ViewController.h"
 
+//@interface ViewController : UIViewController <UIPickerViewDataSource, UIPickerViewDelegate, UITextFieldDelegate>
+
+
 @interface ViewController ()
 
+@property (strong, nonatomic) CLLocationManager *manager;
+@property (weak, nonatomic) IBOutlet UILabel *longLabel;
+@property (weak, nonatomic) IBOutlet UILabel *latLabel;
+
+
 @end
+
+
 
 @implementation ViewController
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    [self setUpLocation];
+}
+
+
+- (void) setUpLocation {
+ 
+    self.manager = [CLLocationManager new];
+    self.manager.delegate = self;
+    [self.manager requestWhenInUseAuthorization];
+    self.manager.desiredAccuracy = kCLLocationAccuracyHundredMeters;
+}
+
+
+- (void)startLocation {
+    [self.manager startUpdatingLocation];
+}
+
+
+- (IBAction)click:(UIButton *)sender {
+    [self startLocation];
+    
+}
+
+
+/*
+ threee different methods
+*/
+- (void)locationManager:(CLLocationManager *)manager didUpdateLocations:(NSArray *)locations {
+    
+    NSLog(@"hit");
+    [self.manager stopUpdatingLocation];
+    
+    CLLocation *newLocation = [locations objectAtIndex:locations.count - 1];
+    NSLog(@"%@", newLocation);
+
+    CLLocation newLocation.coordinate.latitude
+    
+    NSString *latitude = [[NSString alloc] initWithFormat:@"%g°", newLocation.coordinate.latitude];
+    NSString *longitude = [[NSString alloc]initWithFormat:@"%g°", newLocation.coordinate.longitude];
+    [self.longLabel setText:longitude];
+    [self.latLabel setText:latitude];
+    
+}
+
+- (void)locationManager:(CLLocationManager *)manager didUpdateHeading:(CLHeading *)newHeading {
+    
+    NSLog(@"hit2");
+    
 }
 
 - (void)didReceiveMemoryWarning {
